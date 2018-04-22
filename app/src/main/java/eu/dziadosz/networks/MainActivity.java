213443e70e95cd5f5b12695f8bb43ee810a89f3a
Radosław <radoslaw@dziadosz.eu>
2018-04-22 13:29:11 +0200

    IP Calculator view

--- app/src/main/java/eu/dziadosz/networks/MainActivity.java
+++ app/src/main/java/eu/dziadosz/networks/MainActivity.java
@@ -3,11 +3,15 @@ package eu.dziadosz.networks;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import org.apache.commons.net.util.SubnetUtils;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        SubnetUtils subnet = new SubnetUtils("192.168.0.3/31");
+        
     }
 }
